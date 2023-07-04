6ec97c0e9cea22e930f519b868225fc519192a0f
Mikhail Dmitriev <mikdmitr@gmail.com>
2023-07-04 23:31:13 +0300

    Initial commit

---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpGet("[action]")]
+        public async Task<string> GetTest()
+        {
+            var test = "Test messge";
+            
+            _logger.LogTrace("Trace");
+            _logger.LogInformation("Information");
+            _logger.LogError("Error");
+            _logger.LogCritical("Critical");
+            
+
+            return test;
+        }
+    }
+}
